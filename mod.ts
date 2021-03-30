6630b4fe3017f9a7ad723aaa437cc4685ca568f2
Isana Miyake <58712884+isanasan@users.noreply.github.com>
2021-03-30 14:51:09 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
-
-serve({
-  "/": () => new Response("i am isanasan", {headers: {"content-type": "text/html; charset=utf-8"}}),
-  404: () => new Response("ひゃっはーーーーーーーーーーーー", {headers: {"content-type": "text/html; charset=utf-8"}}),
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
 });
